71627552e36f529dda3d4145390ece39050838a3
Developer <softwaremaestro16@gmail.com>
2026-06-06 12:43:49 +0300

    Extend implementation sequencing to validator and resource phases

--- app/params/implementation_sequencing_test.go
+++ app/params/implementation_sequencing_test.go
@@ -11,7 +11,10 @@ func TestDefaultImplementationSequencingPhase0AndPhase1Ready(t *testing.T) {
 	report := BuildImplementationSequencingReport(nil, nil)
 	require.True(t, report.Passed, report.Failed)
 	require.True(t, report.ReadyForPhase1)
+	require.True(t, report.ReadyForPhase2)
+	require.True(t, report.ReadyForPhase3)
 	require.True(t, report.ReadyForIncentiveChanges)
+	require.False(t, report.ReadyForResourcePricing)
 	require.Len(t, report.Phases, 2)
 	require.Len(t, report.Phases[0].Tasks, 7)
 	require.Len(t, report.Phases[1].Tasks, 7)
@@ -19,6 +22,23 @@ func TestDefaultImplementationSequencingPhase0AndPhase1Ready(t *testing.T) {
 	require.Equal(t, SequencingStatusReady, report.Phases[1].Status)
 	require.Contains(t, report.GovernanceSummary, "phase0=ready")
 	require.Contains(t, report.GovernanceSummary, "phase1=ready")
+}
+
+func TestFullImplementationSequencingPhase2AndPhase3Ready(t *testing.T) {
+	report := BuildFullImplementationSequencingReport(nil, nil, nil, nil)
+	require.True(t, report.Passed, report.Failed)
+	require.True(t, report.ReadyForPhase1)
+	require.True(t, report.ReadyForPhase2)
+	require.True(t, report.ReadyForPhase3)
+	require.True(t, report.ReadyForIncentiveChanges)
+	require.True(t, report.ReadyForResourcePricing)
+	require.Len(t, report.Phases, 4)
+	require.Len(t, report.Phases[2].Tasks, 7)
+	require.Len(t, report.Phases[3].Tasks, 7)
+	require.Equal(t, EconomicSequencePhase2, report.Phases[2].PhaseID)
+	require.Equal(t, EconomicSequencePhase3, report.Phases[3].PhaseID)
+	require.Contains(t, report.GovernanceSummary, "phase2=ready")
+	require.Contains(t, report.GovernanceSummary, "phase3=ready")
 }
 
 func TestImplementationSequencingBlocksPhase1WhenMeasurementMissing(t *testing.T) {
@@ -50,6 +70,63 @@ func TestImplementationSequencingBlocksIncentiveChangesWhenSafetyPathDisconnecte
 	require.Equal(t, SequencingStatusBlocked, report.Phases[1].Status)
 	require.Contains(t, report.Failed, EconomicSequencePhase1+":"+SequencingTaskMempoolExecutionFeeValidationAlignment+"_not_ready")
 	require.Contains(t, report.Failed, EconomicSequencePhase1+":controllers_bounded_and_aligned:controller_or_fee_validation_not_reconciled")
+}
+
+func TestImplementationSequencingBlocksPhase2WhenRiskYieldQueriesMissing(t *testing.T) {
+	phase2 := DefaultPhase2SequencingTasks()
+	for i := range phase2 {
+		if phase2[i].Name == SequencingTaskRiskAdjustedYieldEstimates {
+			phase2[i].Queryable = false
+		}
+	}
+
+	report := BuildFullImplementationSequencingReport(nil, nil, phase2, nil)
+	require.False(t, report.Passed)
+	require.True(t, report.ReadyForPhase2)
+	require.False(t, report.ReadyForPhase3)
+	require.False(t, report.ReadyForIncentiveChanges)
+	require.False(t, report.ReadyForResourcePricing)
+	require.Equal(t, SequencingStatusBlocked, report.Phases[2].Status)
+	require.Contains(t, report.Failed, EconomicSequencePhase2+":"+SequencingTaskRiskAdjustedYieldEstimates+"_not_ready")
+	require.Contains(t, report.Failed, EconomicSequencePhase2+":delegator_risk_yield_data_queryable:delegator_query_surface_not_ready")
+}
+
+func TestImplementationSequencingBlocksPhase2WhenEpochSelectionNotDeterministic(t *testing.T) {
+	phase2 := DefaultPhase2SequencingTasks()
+	for i := range phase2 {
+		if phase2[i].Name == SequencingTaskEpochBasedSelectionProduction {
+			phase2[i].Reconciled = false
+		}
+	}
+
+	report := BuildFullImplementationSequencingReport(nil, nil, phase2, nil)
+	require.False(t, report.Passed)
+	require.False(t, report.ReadyForPhase3)
+	require.Contains(t, report.Failed, EconomicSequencePhase2+":"+SequencingTaskEpochBasedSelectionProduction+"_not_ready")
+	require.Contains(t, report.Failed, EconomicSequencePhase2+":active_set_transitions_deterministic_tested:epoch_selection_not_deterministic")
+}
+
+func TestImplementationSequencingBlocksPhase3WhenSpamOrStoragePricingDisconnected(t *testing.T) {
+	phase3 := DefaultPhase3SequencingTasks()
+	for i := range phase3 {
+		switch phase3[i].Name {
+		case SequencingTaskSenderLocalSpamSurcharge:
+			phase3[i].Reconciled = false
+		case SequencingTaskStorageFootprintQueries:
+			phase3[i].Queryable = false
+		}
+	}
+
+	report := BuildFullImplementationSequencingReport(nil, nil, nil, phase3)
+	require.False(t, report.Passed)
+	require.True(t, report.ReadyForPhase3)
+	require.True(t, report.ReadyForIncentiveChanges)
+	require.False(t, report.ReadyForResourcePricing)
+	require.Equal(t, SequencingStatusBlocked, report.Phases[3].Status)
+	require.Contains(t, report.Failed, EconomicSequencePhase3+":"+SequencingTaskSenderLocalSpamSurcharge+"_not_ready")
+	require.Contains(t, report.Failed, EconomicSequencePhase3+":"+SequencingTaskStorageFootprintQueries+"_not_ready")
+	require.Contains(t, report.Failed, EconomicSequencePhase3+":persistent_state_and_spam_directly_priced:resource_abuse_not_directly_priced")
+	require.Contains(t, report.Failed, EconomicSequencePhase3+":state_growth_bounded_by_pricing_telemetry:state_growth_bound_not_queryable")
 }
 
 func TestImplementationSequencingRejectsDuplicateAndMissingTasks(t *testing.T) {
